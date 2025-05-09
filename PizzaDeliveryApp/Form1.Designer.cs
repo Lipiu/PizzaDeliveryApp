3e94f593a2f070591b7c0c1bf6c7baab66718572
Rosu Liviu <rosuliviu10@gmail.com>
2025-05-09 22:08:44 +0300

    added option for ketchup and price for pizzas

--- PizzaDeliveryApp/Form1.Designer.cs
+++ PizzaDeliveryApp/Form1.Designer.cs
@@ -28,12 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             comboBoxPizzaChoice = new ComboBox();
-            label1 = new Label();
-            pictureBox1 = new PictureBox();
+            labelSelectPizza = new Label();
+            pictureBoxPizza = new PictureBox();
             label2 = new Label();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            errorProvider1 = new ErrorProvider(components);
+            labelKetchup = new Label();
+            checkBoxKetchup = new CheckBox();
+            checkBoxSpicy = new CheckBox();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxPizza).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).BeginInit();
             SuspendLayout();
             // 
             // comboBoxPizzaChoice
@@ -45,23 +52,24 @@
             comboBoxPizzaChoice.TabIndex = 0;
             comboBoxPizzaChoice.SelectedIndexChanged += comboBoxPizzaChoice_SelectedIndexChanged;
             // 
-            // label1
+            // labelSelectPizza
             // 
-            label1.AutoSize = true;
-            label1.Location = new Point(12, 242);
-            label1.Name = "label1";
-            label1.Size = new Size(124, 23);
-            label1.TabIndex = 1;
-            label1.Text = "Select pizza 🍕";
+            labelSelectPizza.AutoSize = true;
+            labelSelectPizza.Location = new Point(12, 242);
+            labelSelectPizza.Name = "labelSelectPizza";
+            labelSelectPizza.Size = new Size(124, 23);
+            labelSelectPizza.TabIndex = 1;
+            labelSelectPizza.Text = "Select pizza 🍕";
             // 
-            // pictureBox1
+            // pictureBoxPizza
             // 
-            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(355, 35);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(151, 141);
-            pictureBox1.TabIndex = 2;
-            pictureBox1.TabStop = false;
+            pictureBoxPizza.Image = (Image)resources.GetObject("pictureBoxPizza.Image");
+            pictureBoxPizza.Location = new Point(355, 35);
+            pictureBoxPizza.Name = "pictureBoxPizza";
+            pictureBoxPizza.Size = new Size(151, 141);
+            pictureBoxPizza.TabIndex = 2;
+            pictureBoxPizza.TabStop = false;
+            pictureBoxPizza.Click += pictureBox1_Click;
             // 
             // label2
             // 
@@ -72,14 +80,66 @@
             label2.TabIndex = 3;
             label2.Text = "Lipiu's pizza store";
             // 
+            // errorProvider1
+            // 
+            errorProvider1.ContainerControl = this;
+            // 
+            // labelKetchup
+            // 
+            labelKetchup.AutoSize = true;
+            labelKetchup.Location = new Point(12, 305);
+            labelKetchup.Name = "labelKetchup";
+            labelKetchup.Size = new Size(77, 23);
+            labelKetchup.TabIndex = 4;
+            labelKetchup.Text = "Ketchup?";
+            // 
+            // checkBoxKetchup
+            // 
+            checkBoxKetchup.AutoSize = true;
+            checkBoxKetchup.Location = new Point(155, 305);
+            checkBoxKetchup.Name = "checkBoxKetchup";
+            checkBoxKetchup.Size = new Size(53, 27);
+            checkBoxKetchup.TabIndex = 5;
+            checkBoxKetchup.TabStop = false;
+            checkBoxKetchup.Text = "yes";
+            checkBoxKetchup.UseVisualStyleBackColor = true;
+            checkBoxKetchup.CheckedChanged += checkBoxKetchup_CheckedChanged;
+            // 
+            // checkBoxSpicy
+            // 
+            checkBoxSpicy.AutoSize = true;
+            checkBoxSpicy.Location = new Point(155, 334);
+            checkBoxSpicy.Name = "checkBoxSpicy";
+            checkBoxSpicy.Size = new Size(134, 27);
+            checkBoxSpicy.TabIndex = 6;
+            checkBoxSpicy.Text = "make it spicy?";
+            checkBoxSpicy.UseVisualStyleBackColor = true;
+            checkBoxSpicy.Visible = false;
+            checkBoxSpicy.CheckedChanged += checkBoxSpicy_CheckedChanged;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(255, 128, 128);
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Location = new Point(12, 409);
+            button1.Name = "button1";
+            button1.Size = new Size(144, 30);
+            button1.TabIndex = 7;
+            button1.Text = "Go to checkout";
+            button1.UseVisualStyleBackColor = false;
+            // 
             // Form1
             // 
             AutoScaleMode = AutoScaleMode.None;
             BackColor = Color.FromArgb(255, 128, 128);
             ClientSize = new Size(842, 486);
+            Controls.Add(button1);
+            Controls.Add(checkBoxSpicy);
+            Controls.Add(checkBoxKetchup);
+            Controls.Add(labelKetchup);
             Controls.Add(label2);
-            Controls.Add(pictureBox1);
-            Controls.Add(label1);
+            Controls.Add(pictureBoxPizza);
+            Controls.Add(labelSelectPizza);
             Controls.Add(comboBoxPizzaChoice);
             Font = new Font("Comic Sans MS", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
             FormBorderStyle = FormBorderStyle.FixedDialog;
@@ -87,7 +147,8 @@
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Form1";
             Load += Form1_Load;
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxPizza).EndInit();
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -95,8 +156,13 @@
         #endregion
 
         private ComboBox comboBoxPizzaChoice;
-        private Label label1;
-        private PictureBox pictureBox1;
+        private Label labelSelectPizza;
+        private PictureBox pictureBoxPizza;
         private Label label2;
+        private ErrorProvider errorProvider1;
+        private CheckBox checkBoxKetchup;
+        private Label labelKetchup;
+        private CheckBox checkBoxSpicy;
+        private Button button1;
     }
 }
--- PizzaDeliveryApp/Form1.cs
+++ PizzaDeliveryApp/Form1.cs
@@ -1,36 +1,75 @@
-namespace PizzaDeliveryApp
+﻿namespace PizzaDeliveryApp
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+            errorProvider1 = new ErrorProvider();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             string[] PizzaOptions = new string[]
             {
-                "Pepperoni",
-                "Pepperoni & Feta",
-                "Margherita",
-                "Diavola",
-                "Spicy & Meat",
-                "Quattro Stagioni",
-                "American Spicy",
-                "Quattro formaggi",
-                "Veggie & Mozzarella"
+                "Pepperoni - 28 RON",
+                "Pepperoni & Feta - 35 RON",
+                "Margherita - 25 RON",
+                "Diavola - 30 RON",
+                "Spicy & Meat - 40 RON",
+                "Quattro Stagioni - 30 RON",
+                "American Spicy - 35 RON",
+                "Quattro formaggi - 36 RON",
+                "Veggie & Mozzarella - 30 RON"
             };
 
             comboBoxPizzaChoice.Items.AddRange(PizzaOptions);
             comboBoxPizzaChoice.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBoxPizzaChoice.Width = 240;
             comboBoxPizzaChoice.MaxDropDownItems = 6;
         }
 
         private void comboBoxPizzaChoice_SelectedIndexChanged(object sender, EventArgs e)
         {
             string selectedPizza = comboBoxPizzaChoice.SelectedItem.ToString();
-            MessageBox.Show("You selected: " + selectedPizza);
+
+            if (selectedPizza == null)
+            {
+                errorProvider1.SetError(comboBoxPizzaChoice, "Please select a pizza. Don't you like anything?😭");
+            }
+            else
+            {
+                errorProvider1.SetError(comboBoxPizzaChoice, "");
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hey, stop clicking me!\nOrder some pizza instead🍕");
+        }
+
+        private void checkBoxKetchup_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxKetchup.Checked)
+            {
+                checkBoxSpicy.Visible = true;
+            }
+            else
+            {
+                checkBoxSpicy.Visible = false;
+                checkBoxSpicy.Checked = false;
+            }
+
+        }
+
+        private void checkBoxSpicy_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonOrder_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
